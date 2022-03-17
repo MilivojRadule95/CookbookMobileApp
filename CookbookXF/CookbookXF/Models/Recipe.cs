e6c689a59b75e0fb658eb899286a002d4b841cde
DESKTOP-D84HH4L\Milivoj <milivoj.raodnic@gmail.com>
2022-03-17 21:55:10 +0100

    Models logic

---
+++ CookbookXF/CookbookXF/Models/Recipe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookbookXF.Models
+{
+    internal class Recipe
+    {
+        public string Meal { get; }
+
+        public string RecipeName { get; }
+
+        public string Description { get; }
+
+        public Guid RecipeId { get; }
+
+        public Recipe(string recipeName, string description, Guid recipeId)
+        {
+            RecipeName = recipeName;
+            Description = description;
+
+            if (string.IsNullOrEmpty(recipeName))
+            {
+                throw new InvalidOperationException("Recipe title can't be empty!");
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new InvalidOperationException(nameof(description));
+            }
+        }
+    }
+}
