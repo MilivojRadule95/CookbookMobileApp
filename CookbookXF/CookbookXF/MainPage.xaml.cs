4ed47b365482255aece57e2e273907bc3b258fef
DESKTOP-I9HOOLL\User_9 <dzomarosa@gmail.com>
2022-03-21 13:35:14 +0100

    last push for today

--- CookbookXF/CookbookXF/MainPage.xaml.cs
+++ CookbookXF/CookbookXF/MainPage.xaml.cs
@@ -17,9 +17,75 @@ namespace CookbookXF
          
 
         }
-        
 
-       
-        
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Released(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Released_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Released_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Released_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_4(object sender, EventArgs e)
+        {
+
+        }
     }
 }
